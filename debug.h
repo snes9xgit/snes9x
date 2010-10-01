918a3760100f0213c892fd4fc7047d69c7d2ebf2
OV2 <OV2@sessionclan.de>
2010-10-02 00:49:23 +0200

    Set debug file locations to DEFAULT_DIR

--- debug.h
+++ debug.h
@@ -187,6 +187,9 @@ struct SBreakPoint
 	uint16	Address;
 };
 
+#define ENSURE_TRACE_OPEN(fp,file,mode) \
+	if(!fp) {std::string fn=S9xGetDirectory(DEFAULT_DIR);fn+=SLASH_STR file;fp=fopen(fn.c_str(),mode);}
+
 extern struct SBreakPoint	S9xBreakpoint[6];
 
 void S9xDoDebug (void);
