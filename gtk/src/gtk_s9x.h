d5aae6da636a35f24b7dbe302bc42e9e6f5cf5e1
Brandon Wright <bearoso@gmail.com>
2011-01-22 21:17:17 -0600

    Bump version numbers for GTK+ port.

--- gtk/src/gtk_s9x.h
+++ gtk/src/gtk_s9x.h
@@ -30,8 +30,8 @@
 #define bind_textdomain_codeset(Domain,Codeset) (Codeset)
 #endif /* ENABLE_NLS */
 
-#define SNES9X_GTK_AUTHORS "(c) 2007 - 2010 Brandon Wright (bearoso@gmail.com)"
-#define SNES9X_GTK_VERSION "81"
+#define SNES9X_GTK_AUTHORS "(c) 2007 - 2011 Brandon Wright (bearoso@gmail.com)"
+#define SNES9X_GTK_VERSION "82"
 
 extern Snes9xWindow *top_level;
 extern Snes9xConfig *gui_config;
