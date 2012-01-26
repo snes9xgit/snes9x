804d419b399276b1cdbb14a5a2f718147a7673c0
Brandon Wright <bearoso@gmail.com>
2012-01-26 13:42:27 -0600

    Inline some functions to avoid vtable lookups.

--- apu/bapu/smp/smp.hpp
+++ apu/bapu/smp/smp.hpp
@@ -107,21 +107,21 @@ public:
   unsigned cycle_table_dsp[256];
   uint64 cycle_step_cpu;
 
-  uint8  op_adc (uint8  x, uint8  y);
-  uint16 op_addw(uint16 x, uint16 y);
-  uint8  op_and (uint8  x, uint8  y);
-  uint8  op_cmp (uint8  x, uint8  y);
-  uint16 op_cmpw(uint16 x, uint16 y);
-  uint8  op_eor (uint8  x, uint8  y);
-  uint8  op_inc (uint8  x);
-  uint8  op_dec (uint8  x);
-  uint8  op_or  (uint8  x, uint8  y);
-  uint8  op_sbc (uint8  x, uint8  y);
-  uint16 op_subw(uint16 x, uint16 y);
-  uint8  op_asl (uint8  x);
-  uint8  op_lsr (uint8  x);
-  uint8  op_rol (uint8  x);
-  uint8  op_ror (uint8  x);
+  inline uint8  op_adc (uint8  x, uint8  y);
+  inline uint16 op_addw(uint16 x, uint16 y);
+  inline uint8  op_and (uint8  x, uint8  y);
+  inline uint8  op_cmp (uint8  x, uint8  y);
+  inline uint16 op_cmpw(uint16 x, uint16 y);
+  inline uint8  op_eor (uint8  x, uint8  y);
+  inline uint8  op_inc (uint8  x);
+  inline uint8  op_dec (uint8  x);
+  inline uint8  op_or  (uint8  x, uint8  y);
+  inline uint8  op_sbc (uint8  x, uint8  y);
+  inline uint16 op_subw(uint16 x, uint16 y);
+  inline uint8  op_asl (uint8  x);
+  inline uint8  op_lsr (uint8  x);
+  inline uint8  op_rol (uint8  x);
+  inline uint8  op_ror (uint8  x);
 };
 
 #if defined(DEBUGGER)
