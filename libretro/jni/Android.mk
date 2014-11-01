24d71e3ede3228b815b1421eb7314a7586a6480b
twinaphex <libretro@gmail.com>
2014-11-01 21:35:02 +0100

    Add Makefile.common

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -15,9 +15,15 @@ ifeq ($(TARGET_ARCH),mips)
 LOCAL_CFLAGS += -DANDROID_MIPS
 endif
 
+CORE_DIR := ../..
+
 LOCAL_MODULE    := libretro
-LOCAL_SRC_FILES    =  ../../apu/apu.cpp ../../apu/bapu/dsp/sdsp.cpp  ../../apu/bapu/dsp/SPC_DSP.cpp ../../apu/bapu/smp/smp.cpp ../../apu/bapu/smp/smp_state.cpp  ../../bsx.cpp ../../c4.cpp ../../c4emu.cpp ../../cheats.cpp ../../cheats2.cpp ../../clip.cpp ../../conffile.cpp ../../controls.cpp ../../cpu.cpp ../../cpuexec.cpp ../../cpuops.cpp ../../crosshairs.cpp ../../dma.cpp ../../dsp.cpp ../../dsp1.cpp ../../dsp2.cpp ../../dsp3.cpp ../../dsp4.cpp ../../fxinst.cpp ../../fxemu.cpp ../../gfx.cpp ../../globals.cpp ../../logger.cpp ../../memmap.cpp ../../movie.cpp ../../obc1.cpp ../../ppu.cpp ../../stream.cpp ../../sa1.cpp ../../sa1cpu.cpp ../../screenshot.cpp ../../sdd1.cpp ../../sdd1emu.cpp ../../seta.cpp ../../seta010.cpp ../../seta011.cpp ../../seta018.cpp ../../snapshot.cpp ../../snes9x.cpp ../../spc7110.cpp ../../srtc.cpp ../../tile.cpp ../libretro.cpp
-LOCAL_CXXFLAGS = -DANDROID -DARM -D__LIBRETRO__ -DHAVE_STRINGS_H -DHAVE_STDINT_H -DRIGHTSHIFT_IS_SAR
-LOCAL_C_INCLUDES = ../../ ../../apu/bapu/
+
+include ../Makefile.common
+
+LOCAL_SRC_FILES    =  $(SOURCES_CXX)
+
+LOCAL_CXXFLAGS = -DANDROID -DARM -D__LIBRETRO__ -DHAVE_STRINGS_H -DHAVE_STDINT_H -DRIGHTSHIFT_IS_SAR $(INCFLAGS)
+LOCAL_C_INCLUDES = $(INCFLAGS)
 
 include $(BUILD_SHARED_LIBRARY)
