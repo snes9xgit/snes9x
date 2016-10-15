f4effd36490af69b70c12ecbffd165cd26a77a8e
twinaphex <libretro@gmail.com>
2016-10-15 14:36:18 +0200

    Copyright updates

--- apu/apu.cpp
+++ apu/apu.cpp
@@ -148,6 +148,9 @@
   (c) Copyright 1998 - 2001  John Stiles
   (c) Copyright 2001 - 2011  zones
 
+  Sound emulator code compilation fixes
+  (c) Copyright 2013 - 2016  Daniel De Matteis (UNDER NO CIRCUMSTANCE 
+           WILL COMMERCIAL RIGHTS EVER BE APPROPRIATED TO ANY PARTY)
 
   Specific ports contains the works of other authors. See headers in
   individual files.
