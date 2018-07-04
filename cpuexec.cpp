9a80ecda2a7a8cd3622f46536b6566b5d0d8e940
Brandon Wright <bearoso@gmail.com>
2018-07-04 11:01:00 -0500

    Use delay of 14 instead of 12 cycles for first NMI

--- cpuexec.cpp
+++ cpuexec.cpp
@@ -538,7 +538,7 @@ void S9xDoHEventProcessing (void)
 					// FIXME: triggered at HC=6, checked just before the final CPU cycle,
 					// then, when to call S9xOpcode_NMI()?
 					CPU.NMIPending = TRUE;
-					Timings.NMITriggerPos = 6 + 6;
+					Timings.NMITriggerPos = 14;
 				}
 
 			}
