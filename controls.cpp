e0d88d435f780ca74ab8bde26517efd6180dcbab
CRINKLE-PC\sloan <sloan@lanimals.com>
2018-08-31 19:59:09 -0700

    Swap joypads over Netplay
    Someone smarter than me can probably figure out a more "proper" way to do this, but I hate RetroArch and wanted an easy way to Swap P1 and P2 without both players having to press the button at the same time.

--- controls.cpp
+++ controls.cpp
@@ -2628,6 +2628,15 @@ void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 							S9xSetInfoString("Cannot swap pads: port 2 is not a joypad");
 							break;
 						}
+						
+						if (Settings.NetPlay && data2 != 1) { //data2 == 1 means it's sent by the netplay code
+							if (Settings.NetPlayServer) {
+								S9xNPSendJoypadSwap();
+							} else {
+								S9xSetInfoString("Netplay Client cannot swap pads.");
+								break;
+							}
+						}
 
 						newcontrollers[1] = curcontrollers[0];
 						newcontrollers[0] = curcontrollers[1];
