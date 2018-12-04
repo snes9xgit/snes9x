51d30517125d05bb943a73b5572b69c8cc84d8f1
Brandon Wright <bearoso@gmail.com>
2018-12-03 20:52:12 -0600

    GTK+: Fix data dir name for translations.

--- gtk/src/gtk_s9x.h
+++ gtk/src/gtk_s9x.h
@@ -11,28 +11,7 @@
 #include "gtk_s9xcore.h"
 #include "gtk_s9xwindow.h"
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif /* HAVE_CONFIG_H */
-
-#ifdef ENABLE_NLS
-#include <libintl.h>
-#define _(String) gettext(String)
-#ifdef gettext_noop
-#define N_(String) gettext_noop(String)
-#else
-#define N_(String) (String)
-#endif
-#else /* NLS is disabled */
-#define _(String) (String)
-#define N_(String) (String)
-#define textdomain(String) (String)
-#define gettext(String) (String)
-#define dgettext(Domain,String) (String)
-#define dcgettext(Domain,String,Type) (String)
-#define bindtextdomain(Domain,Directory) (Domain)
-#define bind_textdomain_codeset(Domain,Codeset) (Codeset)
-#endif /* ENABLE_NLS */
+#include <glib/gi18n.h>
 
 #define SNES9X_GTK_AUTHORS "(c) 2007 - 2018 Brandon Wright (bearoso@gmail.com)"
 #define SNES9X_GTK_VERSION "86"
