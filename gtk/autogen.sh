62425f7f43f65c6d7b996126841e6e398edba4ae
Brandon Wright <bearoso@gmail.com>
2018-12-03 20:10:35 -0600

    GTK+: Update gettext and remove intltool.

--- gtk/autogen.sh
+++ gtk/autogen.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-glib-gettextize -f -c
-intltoolize --copy --force --automake
-aclocal
-automake --add-missing --copy
-autoconf
-
-rm -f intltool-extract.in intltool-merge.in intltool-update.in
-    
+autoreconf -i -f
