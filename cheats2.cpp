8387fce0e5fc1bf4d22130f38fbf2e0422e5f581
Brandon Wright <bearoso@gmail.com>
2018-12-29 12:50:24 -0600

    Include ctype.h

--- cheats2.cpp
+++ cheats2.cpp
@@ -4,6 +4,8 @@
    For further information, consult the LICENSE file in the root directory.
 \*****************************************************************************/
 
+#include <ctype.h>
+
 #include "snes9x.h"
 #include "memmap.h"
 #include "cheats.h"
