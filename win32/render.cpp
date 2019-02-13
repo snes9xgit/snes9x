3215788e267c10515d084db8c186281409156f94
Brandon Wright <bearoso@gmail.com>
2019-02-13 17:28:45 -0600

    win32: Fix TVMode in non-directdraw.

--- win32/render.cpp
+++ win32/render.cpp
@@ -859,6 +859,9 @@ void RenderTVMode ( SSurface Src, SSurface Dst, RECT *rect)
         return;
     }
 
+    if (GUI.outputMethod != DIRECTDRAW)
+        snes9x_clear_change_log = 1;
+
     uint8 *nextLine, *finish;
     uint32 colorMask = ~(RGB_LOW_BITS_MASK | (RGB_LOW_BITS_MASK << 16));
     uint32 lowPixelMask = RGB_LOW_BITS_MASK;
