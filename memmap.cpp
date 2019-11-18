14b4b5a9fe2700688668683c30b083275d632542
tryal-star <57671116+tryal-star@users.noreply.github.com>
2019-11-18 21:57:02 +0000

    libretro vita building

--- memmap.cpp
+++ memmap.cpp
@@ -3672,7 +3672,7 @@ void CMemory::ApplyROMFixes (void)
 // BPS % UPS % IPS
 
 // number decoding used for both BPS and UPS
-static uint32 XPSdecode (const uint8 *data, unsigned &addr, unsigned size)
+static uint32 XPSdecode (const uint8 *data, unsigned &&addr, unsigned size)
 {
 	uint32 offset = 0, shift = 1;
 	while(addr < size) {
