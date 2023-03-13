046f348d4ea4c4d122b5b8f9361441e760a0a828
BearOso <bearoso@gmail.com>
2023-03-13 17:59:00 -0500

    Update hqxx filters to adapted version from FFmpeg.
    Compiles much faster and is roughly the same speed.

--- filter/hq2x.h
+++ filter/hq2x.h
@@ -4,13 +4,15 @@
    For further information, consult the LICENSE file in the root directory.
 \*****************************************************************************/
 
+#include <cstdint>
+
 #ifndef _hq2x_h_
 #define _hq2x_h_
 
-bool8 S9xBlitHQ2xFilterInit (void);
-void S9xBlitHQ2xFilterDeinit (void);
-void HQ2X_16 (uint8 *, int, uint8 *, int, int, int);
-void HQ3X_16 (uint8 *, int, uint8 *, int, int, int);
-void HQ4X_16 (uint8 *, int, uint8 *, int, int, int);
+bool S9xBlitHQ2xFilterInit(void);
+void S9xBlitHQ2xFilterDeinit(void);
+void HQ2X_16 (uint8_t *, int, uint8_t *, int, int, int);
+void HQ3X_16 (uint8_t *, int, uint8_t *, int, int, int);
+void HQ4X_16 (uint8_t *, int, uint8_t *, int, int, int);
 
 #endif
