4b345bd008e00f96b99ac4d63738c6a65b6d2888
BearOso <bearoso@gmail.com>
2023-03-30 14:33:05 -0500

    Gtk: Update appimage script.

--- gtk/scripts/makeappimage.sh
+++ gtk/scripts/makeappimage.sh
@@ -11,7 +11,17 @@ if [ ! -f linuxdeploy-x86_64.AppImage ]; then
 fi
 
 DESTDIR=AppDir ninja install
-./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=AppDir
+./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=AppDir \
+	--exclude-library="libX*" \
+	--exclude-library="libglib*" \
+	--exclude-library="libgobject*" \
+	--exclude-library="libgdk_pixbuf*" \
+	--exclude-library="libwayland*" \
+	--exclude-library="libgmodule*" \
+	--exclude-library="libgio*" \
+	--exclude-library="libxcb*" \
+	--exclude-library="libxkbcommon*" \
+	--exclude-library="libdb*"
 
 rm AppDir/snes9x.png
 pushd AppDir
