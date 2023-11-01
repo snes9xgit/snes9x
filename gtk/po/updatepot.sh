38c28e1f9cfde7981043ba55bd7741c27424ec0a
Jake Smarter <JakeSmarter@users.noreply.github.com>
2023-11-01 10:41:00 +0100

    Add de l10n

--- gtk/po/updatepot.sh
+++ gtk/po/updatepot.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd $(dirname "${0}")/..
-xgettext -f po/POTFILES.in --from-code=UTF-8 -o po/snes9x-gtk.pot -kmaybegettext -k_
+xgettext -f po/POTFILES.in \
+         --from-code 'UTF-8' --package-name 'Snes9x' \
+         --copyright-holder 'Brandon Wright and Snes9x contributors' \
+         --msgid-bugs-address 'https://github.com/snes9xgit/snes9x/issues' \
+         -w 80 -o po/snes9x-gtk.pot -kmaybegettext -k_
 cd -
