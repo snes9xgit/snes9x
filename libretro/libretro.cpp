b8655eb9c89fede7803c6b09d219e15665db120d
pstef <3462925+pstef@users.noreply.github.com>
2024-10-20 00:32:51 +0200

    Use the same type on both sides of comparison

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -1148,7 +1148,7 @@ bool retro_load_game(const struct retro_game_info *game)
         if (randomize_memory)
         {
             srand(time(NULL));
-            for(int lcv = 0; lcv < sizeof(Memory.RAM); lcv++)
+            for(size_t lcv = 0; lcv < sizeof(Memory.RAM); lcv++)
                 Memory.RAM[lcv] = rand() % 256;
         }
     }
