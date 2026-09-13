d04b2bad90ae5f54d0f2a0b4fac19f9bc8603d26
Wyatt-James <bomberbeast44@gmail.com>
2026-09-12 19:28:02 -0500

    fxinst: name plot flags, fix plot_8bit testing wrong flag for transparency rejection. Names sourced from BSNES. 8-bit plotting should check PLOT_FREEZEHIGH, not PLOT_OBJECT. Also fixed the logic hierarchy to be more natural.

--- fxemu.cpp
+++ fxemu.cpp
@@ -328,7 +328,7 @@ static void fx_readRegisterSpace (void)
 	else
 		GSU.vScreenSize = (GSU.vScreenHeight / 8) * (256 / 8) * avMult[GSU.vMode];
 
-	if (GSU.vPlotOptionReg & 0x10) // OBJ Mode (for drawing into sprites)
+	if (GSU.vPlotOptionReg & PLOT_OBJECT) // OBJ Mode (for drawing into sprites)
 		GSU.vScreenHeight = 256;
 
 	if (GSU.pvScreenBase + GSU.vScreenSize > GSU.pvRam + (GSU.nRamBanks * 65536))
